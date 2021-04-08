c6f87b098db41a8f0e8823b0ea98a3949ff81a1e
SPANIER Nathan <spaniernathan67@gmail.com>
2021-04-08 12:40:39 +0200

    API to upload image

---
+++ API/main.py
@@ -0,0 +1,30 @@
+import os
+from flask import Flask, request, jsonify
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/api/upload', methods=['POST'])
+def upload_image():
+	if 'file' not in request.files:
+		return jsonify({'error':'file is missing'})
+	file = request.files['file']
+	if file.filename == '':
+		return jsonify({'error':'no image selected for uploading'})
+	if file and allowed_file(file.filename):
+		filename = secure_filename(file.filename)
+		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		return jsonify({'message':'image successfully uploaded'})
+	else:
+		return jsonify({'error':'allowed image types are -> png, jpg, jpeg'})
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
