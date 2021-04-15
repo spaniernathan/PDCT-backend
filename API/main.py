76081d048a51dc9265b71c6e37bb57b194a4a8bc
SPANIER Nathan <spaniernathan67@gmail.com>
2021-04-15 16:23:54 +0200

    dockerfile fix

--- API/main.py
+++ API/main.py
@@ -1,12 +1,25 @@
 import os
 from flask import Flask, request, jsonify
 from werkzeug.utils import secure_filename
+# import tensorflow as tf
+# import cv2 as cv
+# import numpy as np
 
-app = Flask(__name__)
+# Tensorflow init
+MODEL_PATH = './volume/model'
+# physical_devices = tf.config.experimental.list_physical_devices('GPU')
+# f.config.experimental.set_memory_growth(physical_devices[0], True)
+predictModel = None
+
+# App init
 UPLOAD_FOLDER = './volume/uploads'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+#def loadModel():
+	# predictModel = tf.keras.models.load_model(MODEL_PATH)
 
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -21,10 +34,11 @@ def upload_image():
 	if file and allowed_file(file.filename):
 		filename = secure_filename(file.filename)
 		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-		#
 		# Here code that will preprocess the image and push it to the model
+		# img = cv.imread(UPLOAD_FOLDER + file.filename)
+		# img = np.expand_dims(img, axis=0)
 		# Send result from the model back to the app
-		#
+		# predictModel.predict(img)
 		return jsonify({'message':'image successfully uploaded'})
 	else:
 		return jsonify({'error':'allowed image types are -> png, jpg, jpeg'})
