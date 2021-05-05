cab5f04364ec950243765a339571bc6b67945a69
SPANIER Nathan <spaniernathan67@gmail.com>
2021-05-05 15:32:20 +0200

    script

--- API/main.py
+++ API/main.py
@@ -1,28 +1,26 @@
 import os
+import cv2 as cv
+import numpy as np
+import tensorflow as tf
 from flask import Flask, request, jsonify
 from werkzeug.utils import secure_filename
-# import tensorflow as tf
-# import cv2 as cv
-# import numpy as np
 
 # Tensorflow init
-MODEL_PATH = './volume/model'
-# physical_devices = tf.config.experimental.list_physical_devices('GPU')
-# f.config.experimental.set_memory_growth(physical_devices[0], True)
+physical_devices = tf.config.experimental.list_physical_devices('CPU')
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
 predictModel = None
 
 # App init
-UPLOAD_FOLDER = './volume/uploads'
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER'] = './volume/uploads'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.config['MODEL_PATH'] = './volume/model'
 
-#def loadModel():
-	# predictModel = tf.keras.models.load_model(MODEL_PATH)
+def loadModel(app):
+	return tf.keras.models.load_model(app.config['MODEL_PATH'])
 
 def allowed_file(filename):
-	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ['png', 'jpg', 'jpeg']
 
 @app.route('/api/upload', methods=['POST'])
 def upload_image():
@@ -35,11 +33,11 @@ def upload_image():
 		filename = secure_filename(file.filename)
 		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 		# Here code that will preprocess the image and push it to the model
-		# img = cv.imread(UPLOAD_FOLDER + file.filename)
-		# img = np.expand_dims(img, axis=0)
+		img = cv.imread(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		img = np.expand_dims(img, axis=0)
 		# Send result from the model back to the app
-		# predictModel.predict(img)
-		return jsonify({'message':'image successfully uploaded'})
+		return jsonify(predictModel.predict(img))
+		# return jsonify({'message':'image successfully uploaded'})
 	else:
 		return jsonify({'error':'allowed image types are -> png, jpg, jpeg'})
 
@@ -47,3 +45,4 @@ def upload_image():
 if __name__ == '__main__':
 	# Loading the model
 	app.run(debug=True)
+	predictModel = loadModel(app)
