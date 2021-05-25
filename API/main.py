44eb7b6de88fe42d4ddc37e5211f1f129a005e17
SPANIER Nathan <spaniernathan67@gmail.com>
2021-05-25 17:49:14 +0200

    reduced execution time to 2-3 seconds by loading model in global variable

--- API/main.py
+++ API/main.py
@@ -7,24 +7,26 @@ from werkzeug.utils import secure_filename
 
 # Tensorflow init
 physical_devices = tf.config.experimental.list_physical_devices('CPU')
-#tf.config.experimental.set_memory_growth(physical_devices[0], True)
-predictModel = None
 
 # App init
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = '/volume/uploads'
-#app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
-app.config['MODEL_PATH'] = '/volume/model'
+app.config['UPLOAD_FOLDER'] = './volume/uploads'
 
-def loadModel(app):
-	return tf.keras.models.load_model(app.config['MODEL_PATH'])
+global predictionModel
+predictionModel = tf.keras.models.load_model('./volume/model/best_model')
 
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ['png', 'jpg', 'jpeg']
 
+def processImage(img):
+	h, w = img.shape[:2]
+	margin = min(w, h)
+	crop_img = img[0:margin, 0:margin]
+	res = cv.resize(crop_img, (224, 224), interpolation = cv.INTER_CUBIC)
+	return res
+
 @app.route('/api/upload', methods=['POST'])
 def upload_image():
-	tmp = tf.keras.models.load_model(app.config['MODEL_PATH'])
 	if 'file' not in request.files:
 		return jsonify({'error':'file is missing'})
 	file = request.files['file']
@@ -33,17 +35,14 @@ def upload_image():
 	if file and allowed_file(file.filename):
 		filename = secure_filename(file.filename)
 		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-		# Here code that will preprocess the image and push it to the model
 		img = cv.imread(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-		img = np.expand_dims(img, axis=0)
-		# Send result from the model back to the app
-		return jsonify(str(np.argmax(tmp.predict(img))))
+		cropedImg = processImage(img)
+		cropedImg = np.expand_dims(cropedImg, axis=0)
+		return jsonify(str(np.argmax(predictionModel.predict(cropedImg))))
 	else:
 		return jsonify({'error':'allowed image types are -> png, jpg, jpeg'})
 
 
 
 if __name__ == '__main__':
-	# Loading the model
-	app.run(debug=True)
-	predictModel = loadModel(app)
+	app.run()
